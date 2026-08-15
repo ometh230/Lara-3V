fe1491b28feeabb63949477caa33dab4377d02d9
tharumin <94857655+tharumin@users.noreply.github.com>
2026-08-15 18:07:18 +0530

    💃🏻

--- settings.js
+++ settings.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94703508273' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? '𝕃𝔸ℝ𝔸-𝕄𝔻=jZAlAAoI#mXT4SMQrJ4yqEVHsAjYVKs5zLBYGlJWQyK8OBOzV3kM' : process.env.SESSION_ID, //ADD YOUR SSION ID
-    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'tharumin' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '7PNou9kU4q3naOnt7FnBLtgilWHCbA346nd3' : process.env.GITHUB_AUTH_TOKEN, //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
+    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN, //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
     WELCOME: process.env.WELCOME === undefined ? 'true' : process.env.WELCOME,
     GOODBYE: process.env.GOODBYE === undefined ? 'true' : process.env.GOODBYE
 };
